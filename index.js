1ede4f3e09a8599fe0eb0c4bcdd322076100e9d3
professorleandrocesar <professor@leandrocesar.co>
2023-08-23 10:26:35 -0300

    updt

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const usersModel = require('./models/user');
 
-const usersModel = require('./models/user')
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 mongoose.connect('mongodb+srv://leandro:340209755@cluster0.rbzhjzh.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log('Conectado ao MongoDB');
-}).catch(err => console.error('Erro de conexão com o MongoDB:', err));
+})
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+    })
+    .catch(err => console.error('Erro de conexão com o MongoDB:', err));
 
-const User = mongoose.model("User", usersModel)
+const User = mongoose.model("User", usersModel);
 
 const finder = User.find({}).then(users => {
     console.log(users);
@@ -24,13 +24,19 @@ const finder = User.find({}).then(users => {
     console.log(err);
 })
 
-app.get('/', (req,res)=>{
-    res.json(finder);
-})
+// Defina uma rota para buscar todos os usuários
+app.get('/usuarios', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
 
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao buscar usuários.' });
+    }
+});
 
-
-const porta = process.env.PORT || 3016;
+const porta = process.env.PORT || 3017;
 
 app.listen(porta, () => {
     console.log(`Servidor Express rodando na porta ${porta}`);
