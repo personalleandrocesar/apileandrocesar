67b2f0556323d051ef41aff4aaf7a4b05f9aacf6
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-01-22 00:24:40 -0300

    updt

--- dataFind.js
+++ dataFind.js
@@ -6,17 +6,17 @@ const usersModel = require('./models/user')
 
 mongoose.connect('mongodb+srv://leandro:340209755@cluster0.rbzhjzh.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Conectado ao MongoDB');
-}).catch(err => console.error('Erro de conexão com o MongoDB:', err));
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('Conectado ao MongoDB');
+    }).catch(err => console.error('Erro de conexão com o MongoDB:', err));
 
-const User = mongoose.model("User", usersModel)
+    const User = mongoose.model("User", usersModel)
 
-User.find({}).then(users => {
-    console.log(users);
-}).catch(err => {
-    console.log(err);
-})
+    User.find({}).then(users => {
+        console.log(users);
+    }).catch(err => {
+        console.log(err);
+    })
   
 
